2ed34b30691b4cd10919f0328f74e35e0d690ba9
Matt <matthewjmcivor@gmail.com>
2020-09-14 22:57:57 -0500

    Finished Application

---
+++ homework-nineteen/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer text-center">
+            <span>Employee Directory; Homework 19</span>
+        </footer>
+    )
+}
+
+export default Footer;
